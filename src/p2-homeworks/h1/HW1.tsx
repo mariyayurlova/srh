43f057fc2a944d1ac87bbbc6c8ff9b22762c5a3b
Masha Yurlova <mariya.yurlova@gmail.com>
2020-09-20 22:52:48 +0300

    Update

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Message from "./Message";
 
-const messageData = {
-    avatar: "https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg",
-    name: "Some Name",
-    message: "some text",
-    time: "22:00",
-};
+export type MessageType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string
+}
+
 
 function HW1() {
+
+    const messageData: MessageType = {
+        avatar: "https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg",
+        name: "Some Name",
+        message: "some text",
+        time: "22:00",
+    };
+
     return (
         <div>
             <hr/>
