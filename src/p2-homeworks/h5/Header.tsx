0c3b22c76d7c615aba7106983c051b785d5d007d
Маша Юрлова <mariya.yurlova@gmail.com>
2020-11-24 18:04:58 +0300

    HW5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, {ButtonHTMLAttributes, DetailedHTMLProps, useState} from "react";
+import s from "./Header.module.css";
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+    const [isActive, setActive] = useState<boolean>(false);
+
+    const toggleClass = () => {
+        setActive(!isActive);
+    }
+
+    const closeMenu = () => {
+        setActive(!isActive);
+    }
+
+    const finalClassNameMenu = `${isActive ? s.activeMenu : ""} ${s.menu}`;
+    const finalClassNameBurger = `${isActive ? s.activeBurger : ""} ${s.burger}`;
+
+    return (
+        <div className={s.header}>
+            <div className={finalClassNameBurger} onClick={toggleClass}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className={finalClassNameMenu}>
+                <NavLink to={PATH.PRE_JUNIOR} activeClassName={s.activeLink} onClick={closeMenu}>PRE_JUNIOR</NavLink>
+                <NavLink to={PATH.PRE_JUNIOR_PLUS} activeClassName={s.activeLink}
+                         onClick={closeMenu}>PRE_JUNIOR_PLUS</NavLink>
+            </div>
         </div>
     );
 }
