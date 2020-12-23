0db7d1572e6c34a6ff90c32e0519e1fec0dadcb8
Masha Yurlova <mariya.yurlova@gmail.com>
2020-12-23 23:17:40 +0300

    Updates

---
+++ precache-manifest.2c688db71da7765d5309c32b3ef5d490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a8c39e9837a34105b79d05ea42c328",
+    "url": "/srh/index.html"
+  },
+  {
+    "revision": "ab46b891b624e4abcb3c",
+    "url": "/srh/static/css/main.f95c380f.chunk.css"
+  },
+  {
+    "revision": "392dee4c7f0bb0df63eb",
+    "url": "/srh/static/js/2.30060eb4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/srh/static/js/2.30060eb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab46b891b624e4abcb3c",
+    "url": "/srh/static/js/main.adfa0ace.chunk.js"
+  },
+  {
+    "revision": "8c4d9c0dad04e85add28",
+    "url": "/srh/static/js/runtime-main.a88aafe4.js"
+  }
+]);
